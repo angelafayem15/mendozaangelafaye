39fc7bb8002b626a164a1b1df9f09d4aa57d9022
angelafayem15 <91861367+angelafayem15@users.noreply.github.com>
2021-10-17 20:06:19 +0800

    Add files via upload

---
+++ exeercise1_mendoza.py
@@ -0,0 +1,16 @@
+IDNumber = 6001887
+CompleteName = "Mendoza Angela Faye"
+ContactNumber = 9457704143
+Age = 22
+SubjectsEnrolled = ["Systems Intergration and Architecture Lab and Lec", "Platform Technologies", "Intro to Human Comp Interaction ", "Elective 1 Lec and Lab", "Information Assurance & Security"]
+
+print(IDNumber)
+print(CompleteName)
+print(ContactNumber)
+print(Age)
+print(SubjectsEnrolled[0])
+print(SubjectsEnrolled[1])
+print(SubjectsEnrolled[2])
+print(SubjectsEnrolled[3])
+print(SubjectsEnrolled[4])
+
